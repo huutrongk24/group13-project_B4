2d5894a42efababac6f1dbbca81770860dc63204
huutrongk24 <huutrongk24@gmail.com>
2025-10-19 18:28:42 +0700

    hoat dong 7

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,11 @@
-//backend/routes/user.js
+// backend/routes/user.js
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/users', userController.getUsers);
 router.post('/users', userController.createUser);
+router.put('/users/:id', userController.updateUser);
+router.delete('/users/:id', userController.deleteUser);
 
 module.exports = router;
