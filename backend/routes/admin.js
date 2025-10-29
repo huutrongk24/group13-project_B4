48177a1999c2e00c56a17f7761ca886e1ae86572
huutrongk24 <huutrongk24@gmail.com>
2025-10-29 17:26:52 +0700

    .......

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,9 +1,22 @@
+// const express = require('express');
+// const router = express.Router();
+// const { getAllUsers, deleteUser } = require('../controllers/adminController');
+// const { authMiddleware, adminMiddleware } = require('../middlewares/authMiddleware');
+
+// router.get('/users', authMiddleware, adminMiddleware, getAllUsers);
+// router.delete('/users/:id', authMiddleware, adminMiddleware, deleteUser);
+
+// module.exports = router;
+
+
 const express = require('express');
 const router = express.Router();
 const { getAllUsers, deleteUser } = require('../controllers/adminController');
-const { authMiddleware, adminMiddleware } = require('../middleware/authMiddleware');
+const { authMiddleware, adminMiddleware } = require('../middlewares/authMiddleware');
 
 router.get('/users', authMiddleware, adminMiddleware, getAllUsers);
 router.delete('/users/:id', authMiddleware, adminMiddleware, deleteUser);
 
 module.exports = router;
+
+
